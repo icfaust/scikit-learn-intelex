afb20b3bd2926e6b4c756734e9ab7a6ce5920764
Ian Faust <icfaust@gmail.com>
2024-07-18 13:34:36 +0200

    Update __init__.py

--- daal4py/__init__.py
+++ daal4py/__init__.py
@@ -36,7 +36,10 @@ if "Windows" in platform.system():
             if os.path.exists(dal_root_redist):
                 os.add_dll_directory(dal_root_redist)
                 os.environ["PATH"] = dal_root_redist + os.pathsep + os.environ["PATH"]
-        os.add_dll_directory(path_to_libs)
+        try:
+            os.add_dll_directory(path_to_libs) # Conda default
+        except FileNotFoundError:
+            os.add_dll_directory(path_to_libs.rstrip("bin")+"Scripts") # venv default
         os.add_dll_directory(path_to_oneapi_backend)
     os.environ["PATH"] = path_to_libs + os.pathsep + os.environ["PATH"]
 
