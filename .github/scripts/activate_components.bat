1e5139206012741fdc8792d595deef01212c9a77
Ian Faust <icfaust@gmail.com>
2024-07-23 10:13:18 +0200

    Create activate_components.bat

---
+++ .github/scripts/activate_components.bat
@@ -0,0 +1,30 @@
+@echo off
+rem ============================================================================
+rem Copyright 2024 Intel Corporation
+rem
+rem Licensed under the Apache License, Version 2.0 (the "License");
+rem you may not use this file except in compliance with the License.
+rem You may obtain a copy of the License at
+rem
+rem     http://www.apache.org/licenses/LICENSE-2.0
+rem
+rem Unless required by applicable law or agreed to in writing, software
+rem distributed under the License is distributed on an "AS IS" BASIS,
+rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem See the License for the specific language governing permissions and
+rem limitations under the License.
+rem ============================================================================
+
+rem %1 - dpcpp compiler version
+rem %2 - tbb version
+
+set errorcode=0
+
+rem prepare vc
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall" x64
+rem prepare icx
+call .\dpcpp\compiler\%1\env\vars.bat
+rem prepare tbb
+call .\dpcpp\tbb\%2\env\vars.bat
+rem prepare oneDAL
+call .\__release_win\daal\latest\env\vars.bat
