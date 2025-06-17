f812b65946b13ba0ba842b6c58042169bccc56a4
icfaust <icfaust@gmail.com>
2025-06-16 22:21:53 -0700

    fixes

--- onedal/dal.cpp
+++ onedal/dal.cpp
@@ -24,6 +24,7 @@ namespace oneapi::dal::python {
 /* common */
 #ifdef ONEDAL_DATA_PARALLEL_SPMD
 /* algorithms */
+ONEDAL_PY_INIT_MODULE(policy);
 ONEDAL_PY_INIT_MODULE(covariance);
 ONEDAL_PY_INIT_MODULE(dbscan);
 ONEDAL_PY_INIT_MODULE(ensemble);
@@ -83,6 +84,7 @@ ONEDAL_PY_INIT_MODULE(finiteness_checker);
 
 #ifdef ONEDAL_DATA_PARALLEL_SPMD
 PYBIND11_MODULE(_onedal_py_spmd_dpc, m) {
+    init_policy(m);
     init_covariance(m);
     init_dbscan(m);
     init_decomposition(m);
