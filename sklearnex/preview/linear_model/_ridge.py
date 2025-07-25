b9d15982dcc61f94706c1c435b89a69d2120f037
Ian Faust <icfaust@gmail.com>
2025-07-25 21:40:56 +0200

    Create _ridge.py

---
+++ sklearnex/preview/linear_model/_ridge.py
@@ -0,0 +1,33 @@
+# ==============================================================================
+# Copyright Contributors to the oneDAL Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from sklearn.base import RegressorMixin
+from sklearn.linear_model._ridge import _RidgeClassifierMixin
+from sklearn.lienar_model import RidgeClassifier as _sklearn_RidgeClassifier
+
+from ...linear_model import Ridge
+
+# oneDAL array API enabled based on Ridge version
+class RidgeClassifier(_RidgeClassifierMixin, Ridge):
+    __doc__ = RidgeClassifier.__doc__
+    fit = _sklearnRidgeClassifier.fit
+    __sklearn_tags__ = _sklearnRidgeClassifier.__sklearn_tags__
+
+# remove RegressorMixin from the mro
+RidgeClassifier.__bases__ = tuple(cls for cls in RidgeClassifier.__bases__ if cls is not RegressorMixin)
+
+# Allow for isinstance calls without inheritance changes using ABCMeta
+_sklearn_RidgeClassifier.register(RidgeClassifier)
