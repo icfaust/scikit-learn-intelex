95f36ce9eedc7165271d235fbb1a4f3806a92c76
Ian Faust <icfaust@gmail.com>
2025-07-25 22:19:13 +0200

    Update _ridge.py

--- sklearnex/preview/linear_model/_ridge.py
+++ sklearnex/preview/linear_model/_ridge.py
@@ -15,7 +15,7 @@
 # ==============================================================================
 
 from sklearn.base import RegressorMixin
-from sklearn.linear_model._ridge import _RidgeClassifierMixin
+from sklearn.linear_model._ridge import _BaseRidge, _RidgeClassifierMixin
 from sklearn.lienar_model import RidgeClassifier as _sklearn_RidgeClassifier
 
 from ...linear_model import Ridge
@@ -23,8 +23,33 @@ from ...linear_model import Ridge
 # oneDAL array API enabled based on Ridge version
 class RidgeClassifier(_RidgeClassifierMixin, Ridge):
     __doc__ = RidgeClassifier.__doc__
-    fit = _sklearnRidgeClassifier.fit
     __sklearn_tags__ = _sklearnRidgeClassifier.__sklearn_tags__
+
+    def fit(self, X, y, sample_weight=None):
+        X, y, sample_weight, Y = self._prepare_data(X, y, sample_weight, self.solver)
+        if sklearn_check_version("1.2"):
+            self._validate_params()
+
+        # It is necessary to properly update coefs for predict if we
+        # fallback to sklearn in dispatch
+        if hasattr(self, "_onedal_estimator"):
+            del self._onedal_estimator
+
+        dispatch(
+            self,
+            "fit",
+            {
+                "onedal": self.__class__._onedal_fit,
+                "sklearn": _BaseRidge.fit,
+            },
+            X,
+            Y,
+            sample_weight,
+            )
+        return self
+
+    fit.__doc__ = Ridge.fit.__doc__
+
 
 # remove RegressorMixin from the mro
 RidgeClassifier.__bases__ = tuple(cls for cls in RidgeClassifier.__bases__ if cls is not RegressorMixin)
